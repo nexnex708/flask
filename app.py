ce351c50f8fe2b95c2d41debf9598f9696446232
nexnex708 <naosub20060926@gmail.com>
2025-05-26 08:49:14 +0000

    tokuninashi

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/question_lobby")
+def question_lobby():
+    return render_template("question_lobby.html")
+
+@app.route("/question")
+def question():
+    return render_template("question.html")
